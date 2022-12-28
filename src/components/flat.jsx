89f8345e34aba7e54b2f1237e68676a29a019eba
mosesnjoroge <mosesbnjoroge@outlook.com>
2022-12-28 12:59:01 -0500

    flat component

---
+++ src/components/flat.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Flat from './flat';
+
+const Flatlist = (props)=> {
+   const renderList = () => {
+    return props.flats.map((flat, index) => {
+      return (
+        <Flat
+          flat={flat}
+          key={flat.lat}
+          selected={flat.name === props.selectedFlat.name}
+          index={index}
+          selectFlat={props.selectFlat}
+        />
+      );
+    });
+  };
+}
+
+
+
+export default Flatlist;
